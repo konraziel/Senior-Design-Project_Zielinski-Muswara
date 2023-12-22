15715b6921632f3181eb080ff8ae7132a4052201
Konrad Zielinski <konradzielinski214@gmail.com>
2023-12-22 15:48:47 -0500

    Migration to GD4 for all current prog

--- player.gd
+++ player.gd
@@ -1,16 +1,16 @@
-extends KinematicBody
+extends CharacterBody3D
 
 
 var speed = 7.0
 var gravity = 9.8
-var velocity = Vector3.ZERO
+#var velocity = Vector3.ZERO
 var snap_vector = Vector3.DOWN
 var mouse_sensitivity = 0.002  # radians/pixel
 
 var jump_strength = 4.5
 
-onready var interaction=$PlayerHead/Camera/interaction
-onready var hand = $PlayerHead/Camera/hand
+@onready var interaction=$PlayerHead/Camera3D/interaction
+@onready var hand = $PlayerHead/Camera3D/hand
 
 
 var picked_object
@@ -19,7 +19,7 @@ var pull_strength
 
 func pick_object():
 	var collider = interaction.get_collider()
-	if collider != null and collider is RigidBody:
+	if collider != null and collider is RigidBody3D:
 		picked_object=collider
 		print("smfoe")
 
@@ -32,9 +32,9 @@ func _ready():
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		$PlayerHead.rotate_y(-event.relative.x * mouse_sensitivity)
-		$PlayerHead/Camera.rotate_x(-event.relative.y * mouse_sensitivity)
-		$PlayerHead.rotation.y = wrapf($PlayerHead.rotation.y, deg2rad(0), deg2rad(360))
-		$PlayerHead/Camera.rotation.x = clamp($PlayerHead/Camera.rotation.x, -1.2, 1.2)
+		$PlayerHead/Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
+		$PlayerHead.rotation.y = wrapf($PlayerHead.rotation.y, deg_to_rad(0), deg_to_rad(360))
+		$PlayerHead/Camera3D.rotation.x = clamp($PlayerHead/Camera3D.rotation.x, -1.2, 1.2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -57,9 +57,9 @@ func _process(delta):
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
-	var camera_transform = $PlayerHead/Camera.get_global_transform()
+	var camera_transform = $PlayerHead/Camera3D.get_global_transform()
 	var camera_basis = camera_transform.basis
-	input_vector = camera_basis.xform(input_vector).normalized()
+	input_vector = camera_basis * (input_vector).normalized()
 	
 	velocity.x= input_vector.x *speed
 	velocity.z= input_vector.z *speed
@@ -74,7 +74,12 @@ func _process(delta):
 	elif just_landed:
 		snap_vector = Vector3.DOWN
 	
-	velocity= move_and_slide_with_snap(velocity, snap_vector, Vector3.UP, true)
+	set_velocity(velocity)
+	# TODOConverter3To4 looks that snap in Godot 4 is float, not vector like in Godot 3 - previous value `snap_vector`
+	set_up_direction(Vector3.UP)
+	set_floor_stop_on_slope_enabled(true)
+	move_and_slide()
+	velocity= velocity
 	
 	if picked_object != null:
 		var a = picked_object.global_transform.origin
